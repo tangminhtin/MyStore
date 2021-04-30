e5511bf01a8b79e315543167fe5277b4707f0c97
Minh Tin Tang <minhtintang@gmail.com>
2021-04-30 14:00:49 +0700

    Detail Product

--- components/product/ProductItem.js
+++ components/product/ProductItem.js
@@ -5,7 +5,7 @@ const ProductItem = ({ product }) => {
   const userLink = () => {
     return (
       <>
-        <Link href={""}>
+        <Link href={`/product/${product._id}`}>
           <a className={"btn btn-info w-100 me-1 " + classes.button}>Xem</a>
         </Link>
         <button
@@ -20,41 +20,43 @@ const ProductItem = ({ product }) => {
   };
 
   return (
-    <div className="col">
-      <div className={"card h-100 " + classes.borderCard}>
-        <img
-          src={product.images[0].url}
-          className={"card-img-top " + classes.borderCard}
-          alt={product.images[0].url}
-          style={{ height: "200px" }}
-        />
-        <div className="card-body">
-          <h5
-            className={"card-title " + classes.cardTitle}
-            title={product.title}
-          >
-            {product.title}
-          </h5>
-          <p
-            className={"card-text " + classes.cardText}
-            title={product.description}
-          >
-            {product.description}
-          </p>
-        </div>
-        <div className={"card-footer " + classes.borderCard}>
-          <div className="d-flex justify-content-between">
-            <p className="text-danger mb-1">{product.price}₫</p>
-            {product.inStock > 0 ? (
-              <p className="text-danger mb-1">Kho còn: {product.inStock}</p>
-            ) : (
-              <p className="text-danger mb-1">Hết hàng</p>
-            )}
+    <Link href={`product/${product._id}`}>
+      <div className="col" style={{ cursor: "pointer" }}>
+        <div className={"card h-100 " + classes.borderCard}>
+          <img
+            src={product.images[0].url}
+            className={"card-img-top " + classes.borderCard}
+            alt={product.images[0].url}
+            style={{ height: "200px" }}
+          />
+          <div className="card-body">
+            <h5
+              className={"card-title " + classes.cardTitle}
+              title={product.title}
+            >
+              {product.title}
+            </h5>
+            <p
+              className={"card-text " + classes.cardText}
+              title={product.description}
+            >
+              {product.description}
+            </p>
           </div>
-          <div className="d-flex justify-content-between">{userLink()}</div>
+          <div className={"card-footer " + classes.borderCard}>
+            <div className="d-flex justify-content-between">
+              <p className="text-danger mb-1">{product.price}₫</p>
+              {product.inStock > 0 ? (
+                <p className="text-danger mb-1">Kho còn: {product.inStock}</p>
+              ) : (
+                <p className="text-danger mb-1">Hết hàng</p>
+              )}
+            </div>
+            <div className="d-flex justify-content-between">{userLink()}</div>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
---
+++ pages/product/[id].js
@@ -0,0 +1,72 @@
+import Head from "next/head";
+import { useState } from "react";
+import { getData } from "../../utils/fetchData";
+
+const DetailProduct = (props) => {
+  const [product] = useState(props.product);
+  const [tab, setTab] = useState(0);
+
+  const isActive = (index) => {
+    return tab === index ? " active" : "";
+  };
+
+  return (
+    <div className="row">
+      <Head>
+        <title>Chi tiết sản phẩm</title>
+      </Head>
+      <div className="col-md-6 detail_page">
+        <img
+          src={product.images[tab].url}
+          alt={product.images[tab].url}
+          className="d-block img-thumbnail rounded mt-4 w-100"
+          style={{ height: "350px" }}
+        />
+        <div className="row mx-0" style={{ cursor: "pointer" }}>
+          {product.images.map((img, index) => (
+            <img
+              key={index}
+              src={img.url}
+              alt={img.url}
+              className={`img-thumbnail rounded ${isActive(index)}`}
+              style={{ height: "80px", width: "20%" }}
+              onClick={() => setTab(index)}
+            />
+          ))}
+        </div>
+      </div>
+
+      <div className="col-md-6">
+        <h2 className="text-uppercase">{product.title}</h2>
+        <h5 className="text-danger">{product.price}₫</h5>
+        <div className="d-flex justify-content-between">
+          {product.inStock > 0 ? (
+            <h6 className="text-danger">In Stock: {product.inStock}</h6>
+          ) : (
+            <h6 className="text-danger">Out Stock</h6>
+          )}
+          <h6 className="text-danger">Sold: {product.sold}</h6>
+        </div>
+        <div className="my-2">{product.description}</div>
+        <div className="my-2">{product.content}</div>
+        <button
+          type="button"
+          className="btn btn-success d-block my-3 px-5"
+          onClick={null}
+        >
+          Mua
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export async function getServerSideProps({ params: { id } }) {
+  const res = await getData(`product/${id}`);
+
+  return {
+    props: { product: res.product },
+  };
+}
+
+export default DetailProduct;
